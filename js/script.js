50e2e3f55efeee7bef948487cb628d4cc7dac8bb
Эльунр Гулиев <bart1alen@gmail.com>
2023-02-02 22:37:42 +0500

    add

--- js/script.js
+++ js/script.js
@@ -17,3 +17,11 @@ function PopUpShow(){
 function PopUpHide(){
     $("#popup1").hide();
 }
+
+function PopUpShow2(){
+    $("#popup2").show();
+}
+
+function PopUpHide2(){
+    $("#popup2").hide();
+}
