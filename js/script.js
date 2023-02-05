ef992e2677ee31ebe802b6a6b284cc567fd0abdc
Эльунр Гулиев <bart1alen@gmail.com>
2023-02-05 09:22:59 +0500

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     PopUpHide();
+    PopUpHide2();
     $(".header_burger").click(function(event) {
         $(".header_burger,.header_menu").toggleClass("active");
         $("body").toggleClass("lock");
