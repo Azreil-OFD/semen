129d39c521f3acac8ae56bfa384aacca57c1bf66
Azreil <fantom2413@gmail.com>
2023-02-22 23:49:58 +0500

    fix poisk

--- block/request.php
+++ block/request.php
@@ -4,7 +4,7 @@ function get()
 {
     $curl = curl_init();
     curl_setopt_array($curl, [
-        CURLOPT_URL => "https://gist.githubusercontent.com/Azreil-OFD/72537a38e1a48ddf122c7afe22e90002/raw/e85e73706df6c9be83169c6639ea383c08cf923f/gistfile1.json",
+        CURLOPT_URL => "http://94.198.218.20/api/v1/",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
